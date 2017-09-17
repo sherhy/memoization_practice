9617c8b641daa78199cc833297a9a5258e1c14aa
hyunjin <hyunjin.jeong@toki.waseda.jp>
2017-09-17 22:02:13 +0900

    stil too honest,, trying to find optimisation

--- answer.md
+++ answer.md
@@ -1,3 +1,4 @@
 [Insert the correct output for SecretTest3 here]
 ----
 [Write a brief explanation about how your code works here!]
+conway sequence
--- app/main.py
+++ app/main.py
@@ -2,11 +2,43 @@
 # -*- coding: utf-8 -*-
 
 def main(argv):
-  # このコードは引数と標準出力を用いたサンプルコードです。
-  # このコードは好きなように編集・削除してもらって構いません。
-  # ---
-  # This is a sample code to use arguments and outputs.
-  # Edit and remove this code as you like.
-
+	#i is number, and v is the input(digit)
   for i, v in enumerate(argv):
-    print("argv[{0}]: {1}".format(i, v))
+    sv = int(v)
+    conwaySequence(sv)
+
+sequ = {
+		1: 1,
+	}
+
+def conwaySequence(inte):
+	global sequ
+	if inte == 1: 
+		print 1
+		return 1
+	for i in xrange(2,inte+1):
+		nextSequence = conWay(sequ[i-1])
+		sequ[i]=nextSequence
+	print nextSequence
+	return nextSequence
+
+def conWay(num):
+	if num == 1: return 11
+	numstr = str(num)
+	outPut = ""
+	repCount = 0
+	for digit in numstr:
+		if repCount == 0:
+			store = digit
+			repCount =1
+		elif digit == store:
+			repCount +=1
+		else:
+			outPut = outPut+str(repCount)+store
+			store = digit
+			repCount = 1
+	outPut = outPut+str(repCount)+store
+	return int(outPut)
+
+
+#maybe can write the sequence in a file for loading next time
